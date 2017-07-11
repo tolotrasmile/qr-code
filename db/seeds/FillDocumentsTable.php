4ca0a8f837f229bd341cc77466a12f9a1d8eef63
Tolotra Raharison <tolotra.raharison@gmail.com>
2017-07-11 23:09:11 +0300

    Add documents table

---
+++ db/seeds/FillDocumentsTable.php
@@ -0,0 +1,38 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class FillDocumentsTable extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+
+        $this->table('users')->truncate();
+
+        $faker = \Faker\Factory::create();
+        $documents = [];
+
+        for ($i = 0; $i < 20; ++$i) {
+
+            $timestamp = $faker->unixTime('now');
+
+            $documents[] = [
+                'number' => rand(1, 1000),
+                'name' => $faker->sentence(5),
+                'type' => rand(1, 3),
+                'link' => $faker->slug(3),
+                'created_at' => date('Y-m-d H:i:s', $timestamp),
+                'updated_at' => date('Y-m-d H:i:s', $timestamp)
+            ];
+        }
+        $this->insert('documents', $documents);
+    }
+}
